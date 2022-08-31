0149fd15f98a19a715654884905e88f27ac169ed
Jordy <j.j.c.van.raalte@gmail.com>
2022-08-31 13:57:54 +0200

    Initial backend commit

---
+++ Backend/src/main/java/nl/jordyvanraalte/ergast/ErgastApplication.java
@@ -0,0 +1,13 @@
+package nl.jordyvanraalte.ergast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErgastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErgastApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/nl/jordyvanraalte/ergast/ErgastApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.jordyvanraalte.ergast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErgastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
